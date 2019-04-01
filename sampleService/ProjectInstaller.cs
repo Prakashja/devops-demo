589e80f4f4e66a52a45f912e6d339f3936c0e8d1
Puttuboyana <vinay.p@siemens-healthineers.com>
2019-04-01 16:03:53 +0530

    First Commit

---
+++ sampleService/ProjectInstaller.cs
@@ -0,0 +1,23 @@
+﻿/* -------------------------------------------------------------------------------------------------
+   Restricted - Copyright (C) Siemens Healthcare GmbH/Siemens Medical Solutions USA, Inc., 2019. All rights reserved
+   ------------------------------------------------------------------------------------------------- */
+   
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sampleService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ sampleService/Service1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace sampleService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ConsoleApp1.Program p = new ConsoleApp1.Program();
+            
+
+            //Console.WriteLine("Hello You started a service");
+            //Console.ReadLine();
+            
+        }
+
+        protected override void OnStop()
+        {
+            //Console.ReadLine();
+        }
+    }
+}
